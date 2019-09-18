41de76d2037e4c9ce6db7421a44bc71f4e7d04bf
Jason Webb <zen.webb@gmail.com>
2019-09-17 22:00:41 -0500

    Adding local code demos as backup

---
+++ demos/forms/accessible/scripts.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', function(e) {
+  var buttons = document.querySelectorAll('form .buttons');
+
+  buttons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+      e.preventDefault();
+      showErrors();
+    });
+  });
+});
+
+function showErrors() {
+  var controls = document.querySelectorAll('.control');
+
+  controls.forEach(function(control) {
+    control.classList.toggle('has-errors');
+  });
+}
