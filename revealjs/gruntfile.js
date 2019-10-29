7ffda8074a303a2ca8d3a22212811246ec428225
Jason Webb <jwebb@accessible360.com>
2019-10-29 12:31:49 -0500

    Point Grunt server to root folder for dev

--- revealjs/gruntfile.js
+++ revealjs/gruntfile.js
@@ -5,7 +5,7 @@ module.exports = grunt => {
 	require('load-grunt-tasks')(grunt);
 
 	let port = grunt.option('port') || 8000;
-	let root = grunt.option('root') || '.';
+	let root = grunt.option('root') || '../';
 
 	if (!Array.isArray(root)) root = [root];
 
